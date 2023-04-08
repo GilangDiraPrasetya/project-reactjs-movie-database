3055bbcc8a5bbf5856ebed6cce04daa96086f497
GilangDiraPrasetya <gilangdira123@gmail.com>
2023-04-08 20:24:56 +0700

    first commit

---
+++ src/components/Footer.js
@@ -0,0 +1,14 @@
+const Footer = () => {
+  return (
+    <footer id="main-footer" className="text-white py-5">
+      <div className="row">
+        <div className="col text-center">
+          <p className="lead mb-0">Movie Of The Year</p>
+          <p>All data provided by themoviedb.org API, All Right Reserved</p>
+        </div>
+      </div>
+    </footer>
+  );
+};
+
+export default Footer;
